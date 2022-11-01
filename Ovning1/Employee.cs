67ae0003242de967bb05a8d857b4626ed15f1436
robboten <robboten@gmail.com>
2022-11-01 16:45:32 +0100

    some things left to learn for sure..

---
+++ Ovning1/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning1
+{
+    internal class Employee
+    {
+        public string Id { get; } //are get/set of any use here?
+        public string Name; //are get/set of any use here?
+        public Employee(string name, string id)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Ovning1
+﻿//1 -Classes: Employee (Company, Menu)
+//2 - attr: name, salary, (role); methods: SetEmployee, GetEmployee (by name/id), UpdateEmployee, RemoveEmployee, ListEmployees
+//3
+using System;
+
+namespace Ovning1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //make an array with created employees - append for each?
+            SetEmployee();
+
+            //not working atm...
+            //GetEmployeeById(Id);
+            //GetEmployeeByName(Name);
+
+            //GetEmployees() - loop thru array with employees to get info...
         }
+
+        static void SetEmployee()
+        {
+            //should this return employee?
+            Console.WriteLine("Input new employee:");
+            Console.Write("Employee name:");
+            string iName = Console.ReadLine();
+            Guid guid = Guid.NewGuid();
+            string idStr = guid.ToString();
+
+            if (iName != null)
+            {
+                Employee employee = new Employee(iName, idStr);
+                Console.WriteLine(employee.Name +" created with id: "+ employee.Id);
+            } 
+            else 
+            {
+                Console.WriteLine("Not a valid input");
+                //not sure how to deal with a null return value..
+            }
+            
+        }
+
+        static string GetEmployeeById(string Id)
+        {
+            return Id;
+        }
+
+        static string GetEmployeeByName(string Name)
+        {
+            return Name;
+        }
+
+        static string GetEmployees() {
+            return "list of employees...";
+        }
+
     }
 }
