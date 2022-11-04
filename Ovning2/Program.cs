948e124034af80b3a03c9a1c6ea6de82393b2966
robboten <robboten@gmail.com>
2022-11-04 09:55:16 +0100

    cleaning up the code some

--- Ovning2/Program.cs
+++ Ovning2/Program.cs
@@ -22,21 +22,17 @@ namespace Ovning2
         //4. Bygg ut menyn med val att exekvera de övriga övningarna.
 
 
-
         static class MenuItem
         {
             public const char First = '1';
             public const char Second = '2';
             public const char Third = '3';
             public const char Fourth = '4';
-            public const char Quit = 'q';
+            public const char Quit = '0';
         }
 
         static void Main(string[] args)
         {
-            //ConsoleKeyInfo result = Console.ReadKey(true);
-            //Console.WriteLine(result.Key.ToString());
-
             Console.Clear();
 
             while (true)
@@ -45,96 +41,37 @@ namespace Ovning2
                 Console.WriteLine("Huvudmeny");
                 Console.WriteLine("---------");
                 Console.WriteLine("Välj ett av följande val:");
-                Console.WriteLine("1: Se pris baserat på din ålder");
-                Console.WriteLine("2: Beräkna pris för ett sällskap");
-                Console.WriteLine("3: Mening till ord");
-                Console.WriteLine("4: Tredje ordet");
-                Console.WriteLine("q: Avsluta");
+                Console.WriteLine($"{MenuItem.First}: Se pris baserat på din ålder");
+                Console.WriteLine($"{MenuItem.Second}: Beräkna pris för ett sällskap");
+                Console.WriteLine($"{MenuItem.Third}: Stycka upp mening till ord");
+                Console.WriteLine($"{MenuItem.Fourth}: Tredje ordet");
+                Console.WriteLine($"{MenuItem.Quit}: Avsluta");
 
                 char command = Console.ReadKey(true).KeyChar;
                 switch (command)
                 {
-                    case MenuItem.First: Console.Clear(); CheckAgeAlt(); break;
+                    case MenuItem.First: Console.Clear(); CheckAgeAlt(true); break;
                     case MenuItem.Second: Console.Clear(); CalcPrice(); break;
-                    case MenuItem.Third: Console.Clear(); Parrot(); break;
+                    case MenuItem.Third: Console.Clear(); Dahmer(); break;
                     case MenuItem.Fourth: Console.Clear(); ThirdWord(); break;
                     case MenuItem.Quit: return;
                     default: Console.WriteLine($"\nVälj ett av valen 1-4 eller {MenuItem.Quit}\n"); break;
-                }
-                
+                } 
             }
         }
 
-        //alternative with ternary operator returning tuple with int and string only. Good - clean. Bad - ?
-        static int CheckAgeAlt()
-        {
-            (int,string) GetPrice2(uint value)
-            {
-                return
-                    (value > 100) ? (0, "Gratis") :
-                    (value > 64) ? (90, "Pensionärspris") :
-                    (value > 20) ? (120, "Standardpris") :
-                    (value > 5) ? (80, "Ungdomspris") :
-                    (value > 0) ? (0, "Gratis") :
-                    (120,"Standardpris");
-            }
-            while (true)
-            {
-                Console.Write("Skriv en ålder: ");
-                if (uint.TryParse(Console.ReadLine(), out uint age))
-                {
-                    int price = GetPrice2(age).Item1;
-                    Console.WriteLine($"Priset för {age} år är {price} kr ({GetPrice2(age).Item2})");
-                    Console.WriteLine("Tryck valfri tangent för att fortsätta ...");
-                    Console.ReadKey();
-                    return price;
-                }
-                else
-                {
-                    Console.WriteLine("Skriv åldern i siffror");
-                }
-            }
 
-            //alternative with ternary operator returning ints only. Good - clean. Bad - no nice string if keeping the prices as ints
-            //int GetPrice(uint value)
-            //{
-            //    return
-            //        (value > 100) ? 0 :
-            //        (value > 64) ? 90 :
-            //        (value > 20) ? 120 :
-            //        (value > 5) ? 80 :
-            //        (value > 0) ? 0 :
-            //        120;
-            //}
-            //while (true)
-            //{
-            //    Console.Write("Skriv en ålder: ");
-            //    if (uint.TryParse(Console.ReadLine(), out uint age))
-            //    {
-            //        int price = GetPrice(age);
-            //        Console.WriteLine($"Priset för {age} år är {price} kr");
-            //        Console.WriteLine("Tryck valfri tangent för att fortsätta ...");
-            //        Console.ReadKey();
-            //        return price;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Skriv åldern i siffror");
-            //    }
-            //}
-        }
+        //1. Användaren anger en ålder i siffror
+        //2.Programmet konverterar detta från en sträng till en int
+        //3.Programmet ser om personen är ungdom(under 20 år)
+        //4.Om det ovanstående är sant skall programmet skriva ut: Ungdomspris: 80kr
+        //5.Annars kollar programmet om personen är en pensionär(över 64 år)
+        //6.Om ovanstående är sant skall programmet skruva ut: Pensionärspris: 90kr
+        //7.Annars skall programmet skriva ut: Standardpris: 120k
 
         //alt with old ifs, could be case too maybe? Good - easy to read, more output. Bad - long and unflexible
         static int CheckAge() //1
         {
-            // Användaren anger en ålder i siffror
-            //2.Programmet konverterar detta från en sträng till en int
-            //3.Programmet ser om personen är ungdom(under 20 år)
-            //4.Om det ovanstående är sant skall programmet skriva ut: Ungdomspris: 80kr
-            //5.Annars kollar programmet om personen är en pensionär(över 64 år)
-            //6.Om ovanstående är sant skall programmet skruva ut: Pensionärspris: 90kr
-            //7.Annars skall programmet skriva ut: Standardpris: 120k
-
             int price;
             while (true)
             {
@@ -175,6 +112,68 @@ namespace Ovning2
             }
         }
 
+        //alternative with ternary operator returning tuple with int and string. Good - clean. Bad - ?
+        static int CheckAgeAlt(bool echo)
+        {
+            (int, string) GetPrice2(uint value)
+            {
+                return
+                    (value > 100) ? (0, "Gratis") :
+                    (value > 64) ? (90, "Pensionärspris") :
+                    (value > 20) ? (120, "Standardpris") :
+                    (value > 5) ? (80, "Ungdomspris") :
+                    (value > 0) ? (0, "Gratis") :
+                    (120, "Standardpris");
+            }
+            while (true)
+            {
+                Console.Write("Skriv en ålder: ");
+                if (uint.TryParse(Console.ReadLine(), out uint age))
+                {
+                    int price = GetPrice2(age).Item1;
+                    Console.WriteLine($"Priset för {age} år är {price} kr ({GetPrice2(age).Item2})");
+                    if (echo)
+                    {
+                        Console.WriteLine("Tryck valfri tangent för att fortsätta ...");
+                        Console.ReadKey();
+                    }
+                    return price;
+                }
+                else
+                {
+                    Console.WriteLine("Skriv åldern i siffror");
+                }
+            }
+
+            //alternative with ternary operator returning ints only. Good - clean. Bad - no nice string if keeping the prices as ints
+            //int GetPrice(uint value)
+            //{
+            //    return
+            //        (value > 100) ? 0 :
+            //        (value > 64) ? 90 :
+            //        (value > 20) ? 120 :
+            //        (value > 5) ? 80 :
+            //        (value > 0) ? 0 :
+            //        120;
+            //}
+            //while (true)
+            //{
+            //    Console.Write("Skriv en ålder: ");
+            //    if (uint.TryParse(Console.ReadLine(), out uint age))
+            //    {
+            //        int price = GetPrice(age);
+            //        Console.WriteLine($"Priset för {age} år är {price} kr");
+            //        Console.WriteLine("Tryck valfri tangent för att fortsätta ...");
+            //        Console.ReadKey();
+            //        return price;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Skriv åldern i siffror");
+            //    }
+            //}
+        }
+
         static void CalcPrice() //2
         {
 
@@ -190,7 +189,7 @@ namespace Ovning2
                     int total = 0;
                     for (int i = 0; i < nr; i++)
                     {
-                        total=total+CheckAge();
+                        total=total+ CheckAgeAlt(false);
                     }
                     Console.WriteLine($"Den totala kostnaden för sällskapet är {total} kr");
                     Console.WriteLine("Tryck valfri tangent för att fortsätta ...");
@@ -205,40 +204,32 @@ namespace Ovning2
             }
         }
 
-        static void Parrot() //3
+        static void Dahmer() //3
         {
             //Händelseförloppet visas nedan:
             //1.Användaren anger en godtycklig text
             //2.Programmet sparar texten som en variabel
-            //3.Programmet skriver, via en for-loop ut denna text tio gånger på rad, alltså UTAN
-            //radbrott.Exempel på output: 1.Input, 2.Input, 3.Input osv.
-            
+            //3.Programmet skriver, via en for-loop ut denna text tio gånger på rad, alltså UTAN radbrott.
+            //Exempel på output: 1.Input, 2.Input, 3.Input osv.
 
             Console.Write("Skriv in ett antal ord: ");
-            string str = Console.ReadLine();
-            var lst = str.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            string str = Console.ReadLine(); //if there is no word nothing will happen so not sure if validation is needed here
+            var lst = str.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries); //split on space and remove leftover whitespaces
+            Console.WriteLine();
 
             //alt 1 - no number prefix but simple code and no ugly , at the end
             Console.WriteLine($"Output alt 1: {string.Join(", ",lst)}\n");
-            Console.WriteLine();
 
-            //alt 2 - no number prefix but simple code and no ugly , at the end
+            //alt 2 - has it all but is a bit tricky to understand (nests join, select and lambda into one)
+            //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
             string Prefix = ".";
             string strOutput = string.Join(", ", lst.Select((x,index) => index+1 + Prefix + x));
             Console.WriteLine($"Output alt 2:{strOutput}");
-
-            Console.WriteLine("Output alt 3");
-            var query =lst.Select((lst, index) => new { index, word = lst.Substring(0, index) });
             Console.WriteLine();
 
-            foreach (var obj in query)
-            {
-                Console.Write(obj.index+"."+obj.word);
-            }
-
-            lst = str.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
             //alt 3 - ugly comma at end and long code
-            Console.WriteLine("Output alt 4:");
+            //would also be possible to add a ", " to each word in lst but the last one, before looping out
+            Console.WriteLine("Output alt 3:");
             for (int i = 0; i < (lst.Length); i++)
             {
                 Console.Write($"{i+1}.{ lst[i]}, ");
@@ -273,8 +264,6 @@ namespace Ovning2
                     }
                 }
             }
-
-
         }
 
         //Extra uppgifter för de som har tid över:
